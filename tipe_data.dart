125f10881cf7631cab58428381b0c56996a77eec
SANJI0722 <saepullohepul565@gmail.com>
2022-11-19 15:00:05 +0700

    Delete

--- tipe_data.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int a = 100;
-  double b = 100.00;
-
-  String nama = 'Epul';
-}
--- variable.dart
+++ variable.dart
@@ -1,4 +1,7 @@
 void main() {
   int a = 100;
   String nama = 'Epul';
+  double b = 34.00;
+
+  print(nama);
 }
